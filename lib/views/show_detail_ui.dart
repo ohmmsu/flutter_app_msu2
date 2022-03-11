8713b6dcd1b65828b1037a795295161fbf7a0757
ohmmsu <siamidea@gmail.com>
2022-03-11 10:02:20 +0700

    Code v.2 : Setup

---
+++ lib/views/show_detail_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoeDetailUI extends StatefulWidget {
+  const ShoeDetailUI({Key? key}) : super(key: key);
+
+  @override
+  State<ShoeDetailUI> createState() => _ShoeDetailUIState();
+}
+
+class _ShoeDetailUIState extends State<ShoeDetailUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
