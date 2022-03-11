b47658b4bb49998e5bd6fad590f823d19a596312
ohmmsu <siamidea@gmail.com>
2022-03-11 15:26:28 +0700

    Code v.3 : Lernp

---
+++ lib/views/show_list_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowListUI extends StatefulWidget {
+  const ShowListUI({Key? key}) : super(key: key);
+
+  @override
+  State<ShowListUI> createState() => _ShowListUIState();
+}
+
+class _ShowListUIState extends State<ShowListUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
